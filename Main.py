6543918f23a9563407f5745729098b79221e4b56
amdudda <tk0654wm@minneapolis.edu>
2016-09-02 10:52:48 -0500

    Implement main menu.

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 from CreateDB import *
 from Vocabulary import *
 
+# some static values
+OPTION_F2E = '1'
+OPTION_E2F = '2'
+OPTION_ADD = '3'
+OPTION_QUIT = '4'
+
 '''
 DATABASE SETUP BLOCK - IGNORE UNLESS YOU NEED TO MODIFY DATA.
 '''
@@ -14,12 +20,39 @@ DATABASE SETUP BLOCK - IGNORE UNLESS YOU NEED TO MODIFY DATA.
 '''
 FUNCTIONS USED BY MAIN MODULE
 '''
+def showmenu():
+    options = "1. Test Finnish to English\n" + \
+        "2. Test English to Finnish\n" +  \
+        "3. Add Vocabulary\n" +  \
+        "4. Quit\n"
+    print(options)
+# end showmenu
 
-
+def quizme():
+    while True:
+        showmenu()
+        userinput = raw_input("> ")
+        if userinput == "1":
+            print("not impelemented yet")
+        elif userinput == "2":
+            print("not impelemented yet")
+        elif userinput == "3":
+            print("not impelemented yet")
+        elif userinput == "4":
+            print("Kiitos!")
+            # exit the program
+            exit()
+            break   # just being cautious - no endless loops, please
+        else:
+            # tell the user they made an invalid choice and have them try again
+            print("You have not made a valid selection. Please try again.")
+    # end while loop
+# End quizme
 
 '''
 BODY OF CODE
 '''
-myWord = getVocabByID(99)
-if myWord != None:
-    print(myWord)
+quizme()
+# myWord = getVocabByID(5)
+# if myWord != None:
+#     print(myWord)
